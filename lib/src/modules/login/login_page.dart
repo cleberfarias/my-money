c4768e265270272350d5e7410d8097d445a84328
cleberfarias <cleber_afd@hotmail.com>
2024-03-14 22:02:32 -0300

    login

---
+++ lib/src/modules/login/login_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:my_money/src/shared/colors/app_colors.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.appPageBackground,
+      body: SingleChildScrollView,
+    );
+  }
+}

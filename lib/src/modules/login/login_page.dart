7cef3b369afe874b3a564836dd64fc9fc402cd16
cleberfarias <cleber_afd@hotmail.com>
2024-03-17 21:44:30 -0300

    ,

--- lib/src/modules/login/login_page.dart
+++ lib/src/modules/login/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_money/src/shared/colors/app_colors.dart';
+import 'package:my_money/src/shared/colors/components/app_logo_title.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -9,15 +10,63 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController loginController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.appPageBackground,
       body: SingleChildScrollView(
         child: SafeArea(
-          child: ,))
-
-      
+            child: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 140, left: 25, right: 25),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const AppLogoTitle(
+                  title: "Login",
+                  iconSize: 80,
+                  titleSize: 20,
+                ),
+                TextFormField(
+                  enabled: true,
+                  controller: loginController,
+                  textAlign: TextAlign.start,
+                  autofocus: false,
+                  keyboardType: TextInputType.text,
+                  decoration: const InputDecoration(label: Text("Login")),
+                ),
+                TextFormField(
+                  enabled: true,
+                  controller: passwordController,
+                  textAlign: TextAlign.start,
+                  autofocus: false,
+                  obscureText: true,
+                  decoration: const InputDecoration(label: Text("Senha")),
+                ),
+                Padding(padding: const EdgeInsets.only(top: 20, bottom: 10,
+                ),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: ElevatedButton(
+                    onPressed: (){},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.green)
+                      elevation: MaterialStateProperty.all(0),
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50)))),
+                    child: const Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),)),
+                   ),
+                )
+              ],
+            ),
+          ),
+        )),
+      ),
     );
   }
 }

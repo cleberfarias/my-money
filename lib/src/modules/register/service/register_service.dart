f9cb5449b1d7f024983a757a3c4272eb96ad72f8
cleberfarias <cleber_afd@hotmail.com>
2024-04-02 13:18:51 -0300

    atualização

--- lib/src/modules/register/service/register_service.dart
+++ lib/src/modules/register/service/register_service.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/foundation.dart';
 import 'package:my_money/src/modules/register/model/register_data_modal.dart';
-
 import 'package:my_money/src/modules/register/repositories/register_repository.dart';
 
 class RegisterService {

f6085d351ef2449aecee05c3f8f8dc3037ab8350
cleberfarias <cleber_afd@hotmail.com>
2024-04-03 17:48:27 -0300

    atualização do repositori

--- lib/src/modules/register/repositories/register_repository.dart
+++ lib/src/modules/register/repositories/register_repository.dart
@@ -3,7 +3,7 @@ import 'package:my_money/src/http/app_dio.dart';
 import 'package:my_money/src/modules/register/model/register_data_modal.dart';
 
 class RegisterRepository {
-  String baseURL = "http://192.168.31.96:3099/user";
+  String baseURL = "http://10.0.0.102:3099/user";
 
   Future<Response<Map<String, dynamic>>> sendRegisterData(
       RegisterDataModel registerData) async {
